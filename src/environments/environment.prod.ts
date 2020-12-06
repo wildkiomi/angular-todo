159cad0c70ebc5b0e99ca2f13c8880864f307a7b
wildkiomi <whisann.lie14@gmail.com>
2020-12-06 15:39:01 +0300

    Fixed firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBSFH2oXA6fMYsY-lbM9-ghXNevFjIB5aA',
+    authDomain: 'angulartodo-336d3.firebaseapp.com',
+    databaseURL: 'https://angulartodo-336d3-default-rtdb.firebaseio.com',
+    projectId: 'angulartodo-336d3',
+    storageBucket: 'angulartodo-336d3.appspot.com',
+    messagingSenderId: '351560926339',
+    appId: '1:351560926339:web:cfa825b4928791e31747b6',
+  },
 };
