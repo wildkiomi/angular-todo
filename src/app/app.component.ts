8c2857d1013474065d6902fad8a1a8f372eb9a9d
Hanna Shelesaya <hanna.shelesnaya@ecomcharge.com>
2020-12-07 15:01:37 +0300

    Issue with observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
 import { AuthService } from './services/auth.service';
 
 @Component({
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,33 +1,49 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { Todo } from '../todo';
+import { AuthService } from './auth.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  user;
+
+  firestoreCollection;
+
   constructor(
     private firestore: AngularFirestore,
-  ) { }
-
-  public firestoreCollection: AngularFirestoreCollection<any> = this.firestore.collection('todoList');
-
-  public getTodoList(): Observable<any> {
-    return this.firestoreCollection.snapshotChanges()
-      .pipe(
-        map(actions => {
-          return actions.map(p => {
-            const todo = p.payload.doc;
-            const id = todo.id;
-            const data = todo.data() as Todo;
-            return { ...data, id };
-          });
+    private authService: AuthService,
+  ) {
+    this.authService.getUser().subscribe((user) => {
+        if (user) {
+          this.user = user;
+          this.firestore.collection('users').doc(user.uid).set({});
+          this.firestoreCollection = this.firestore.collection(`users/${user.uid}/todoList`);
         }
-        )
-      );
+      }
+    )
+  }
+
+
+  public getTodoList(): Observable<any> | any {
+    return this.firestoreCollection.snapshotChanges()
+    .pipe(
+      map((actions: any) => {
+        return actions.map(p => {
+          console.log(p)
+          const todo = p.payload.doc;
+          const id = todo.id;
+          const data = todo.data() as Todo;
+          return { ...data, id };
+        });
+      }
+      ),
+      catchError((err) => of([]))
+    );
   }
 
   public complete(todo: Todo): void {
